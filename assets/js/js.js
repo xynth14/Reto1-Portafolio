20898d58d7878847a66688639dba3dd9ac2cedcf
Cynthia Quispe <cyn19870@gmail.com>
2017-03-16 09:43:22 -0500

    subiendo cambios

--- assets/js/js.js
+++ assets/js/js.js
@@ -10,16 +10,67 @@ function Resource(idAgent,idResource, resource){
 	this.resource = resource;
 }
 
+myAgents.forEach(function(e){
+	var mostrar = "";
+	var clase = "";
+	if(e.state=='idle'){
+		clase += "idle";
+		mostrar += '<div class="divDeny">' +
+		            	'<img src="assets/images/deny-icon.png" class="deny">' +
+		            	'<u>' + e.permission + '</u>' +
+		            '</div>';
+	}else{
+		clase += "building";
+		mostrar += '';
+	}
+	
+
+	$(".div-col-1").append('<div class="resources ' + clase + '">' +
+					            '<div class="divCircle">' +
+					            	'<div class = "circle"></div>' +
+					            '</div>' +
+					            '<div class = "contenido">' +
+					                '<div class = "contenido-lineas1">' +
+					                    '<span>' + e.name + '.' + e.dominio + '</span>' +
+					                    '<span class = "cont-resources">' + e.state + ' </span>' +
+					                    '<span class = "cont-resources">' + e.ip + ' </span>' +
+					                    '<span class = "cont-resources">' + e.url + ' </span>' +
+					                '</div>' +
+					                '<div class="contenido-lineas2" id="cont'+e.id+'">' +
+					                    '<strong>+</strong><a href="#" class="specifyResource" id="' + e.id + '">Specify Resources</a>' +
+					                    '<span class="cont-resources">Resources:</span>' +                    
+					        	    '</div>' +
+					            '</div>' +
+					            mostrar +
+					        '</div>');
+
+});
 
 var specifyResource = document.getElementsByClassName("specifyResource");
-
+var idResource = 0;
+var agents = [];
 
 for(var i=0; i<specifyResource.length;i++){
 	specifyResource[i].onclick = function(){
-		//var idR = document.getElementsByClassName("idR");
+		
 		var txtRecursos = prompt("(Separate multiple resources name with commas)");
-		//alert(e);
-		alert(this.id);
+		var arrRecursos = txtRecursos.split(",");
+		for(j in arrRecursos){
+			idResource++;
+			var resourceAdd = new Resource(this.id,idResource, arrRecursos[j].trim());
+			agents.push(resourceAdd);
+
+			myAgents.map(function(e){
+				//alert(e.id.toString());
+				//alert(agents[j].idAgent);
+				if(e.id.toString() == agents[j].idAgent){
+					//alert(true);
+					$("#cont"+e.id).append('<span class = "cont-resources2">' + arrRecursos[j].trim() + ' <span id="btneliminar">X</span></span>');
+				}
+			});
+			
+		}
+		
 	};
 		
 
